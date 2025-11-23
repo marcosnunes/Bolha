1bb1da66a7d0ea2532fbba6d8cc3701607764533
marcos.lindolpho <marcos.lindolpho@gmail.com>
2025-11-23 23:43:32 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Força o Vite a procurar os arquivos .env na pasta raiz do projeto.
+  envDir: './',
 })
