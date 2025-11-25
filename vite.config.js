d6c380f0cd54f24000b6f55f93376dd316a058c1
marcos.lindolpho <marcos.lindolpho@gmail.com>
2025-11-25 04:09:27 +0000

    feat: Adiciona 17 novos níveis, corrige bugs e otimiza build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Agrupa todas as dependências de node_modules em um único chunk 'vendor'
+            // Módulos grandes e mais complexos podem ser separados em seus próprios chunks
+            if (id.includes('@mui')) {
+              return 'vendor_mui';
+            }
+            if (id.includes('firebase')) {
+              return 'vendor_firebase';
+            }
+            if (id.includes('react-router-dom') || id.includes('react-dom') || id.includes('react')) {
+                return 'vendor_react';
+            }
+            return 'vendor'; // chunk para todas as outras dependências
+          }
+        },
+      },
+    },
+  },
+});
