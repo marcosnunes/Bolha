35742f053d6d82a0b09dbe3494ac85262a695a4e
marcosnunes <marcos.nunes.lph@outlook.com>
2026-01-09 00:10:43 -0300

    Update

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -68,7 +68,7 @@ function ProfileModal({ userToDisplay, onClose, onHideUser, onShowUser, onEditPr
                 >
                   {!userProfile.photoURL && userProfile.nickname.charAt(0).toUpperCase()}
                 </Avatar>
-                <VerificationBadge isVerified={userProfile.isVerified} avatarSize={100} customSx={{ bottom: '2px', right: '2px' }} />
+                <VerificationBadge isVerified={userProfile.isVerified} avatarSize={100} customSx={{ bottom: '8px', right: '8px' }} />
               </Box>
             ) : (
               <Skeleton variant="circular" width={100} height={100} />
