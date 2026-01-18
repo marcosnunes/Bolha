ee893859ce67c3c1499c70a0f72f6171f74513a3
marcosnunes <marcos.nunes.lph@outlook.com>
2026-01-18 06:47:37 -0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     minify: 'terser',
     terserOptions: {
       compress: {
-        drop_console: true,
+        drop_console: false,
       },
     },
     rollupOptions: {
